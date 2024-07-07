e907254cc07759ff47553be03cb6adae0836a698
Pierre <p.couturier3400@gmail.com>
2024-07-07 10:29:31 +0200

    feat: add new version of skills icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import Icons from 'unplugin-icons/vite';
+import { FileSystemIconLoader } from 'unplugin-icons/loaders';
 
 export default defineConfig({
 	plugins: [
         sveltekit(),
         Icons({
+            customCollections: {
+              'skills': FileSystemIconLoader("./assets")
+            },
             compiler: 'svelte'
         })]
 });
